26024caf61389212bc00b43445480d06735c2857
SebastianBaeza <sebastian.baezaa@usm.cl>
2022-10-20 18:26:56 -0300

    Pruebas que "funcionan"

--- tarea_2_copy.c
+++ tarea_2_copy.c
@@ -7,8 +7,9 @@
 #include <unistd.h>
 #include <time.h>
 //las 28 piezas
-int piezas[7][2] = {{6,6},{6,5},{6,4},{6,3},{6,2},{6,1},{6,0}};
+int piezas[28][2] = {{6,6},{6,5},{6,4},{6,3},{6,2},{6,1},{6,0}};
 //mazo de cada jugador
+/*
 struct mazo{
     int pieza1[2];
     int pieza2[2];
@@ -18,6 +19,7 @@ struct mazo{
     int pieza6[2];
     int pieza7[2];
 };
+*/
 //el tablero de juego, solo me importa lo que hay a la izq y a la der,
 //si pongo una pieza en la izq reemplazo el valor
 struct colocar{
@@ -26,19 +28,19 @@ struct colocar{
 };
 //quiero rellenar los mazos de los jugadores, el random es para que me de un numero del 0 al 27 para rellenar el mazo con la pieza
 //en la posición del numero del arreglo piezas
-void crear_piezas(struct mazo *jugador){
+void crear_piezas(int mazo[7][2]){
     int posicion = 0;
-    printf("%s","hasta aca bien-1");
-    // srand(time(NULL)); pass the srand() parameter  
-    // printf("\n");  print the space 
-    printf("\n%s","hasta aca bien0"); 
+    //printf("%s","hasta aca bien-1\n");
+    srand(time(NULL)); //pass the srand() parameter  
+    //printf("\n");  //print the space 
+    //printf("\n%s","hasta aca bien0\n"); 
     for (int i = 0; i < 7; i++)  
     {  
-         posicion=rand() % 7; //cambiar a 27
-         printf("%s","hasta aca bien1");
-         jugador->pieza1[0]=piezas[posicion][0];
-         jugador->pieza1[1]=piezas[posicion][1];
-         printf("%s","hasta aca bien2");
+        posicion = rand() % 7; //cambiar a 27
+        //printf("%s","hasta aca bien1\n");
+        mazo[i][0] = piezas[posicion][0];
+        mazo[i][1] = piezas[posicion][1];
+        //printf("%s","hasta aca bien2\n");
     }  
 
 }
@@ -68,9 +70,16 @@ void inicializar_jugador(struct mazo *jugador){
 */
 //el main
 int main(){
-    struct mazo *jugador1;
+    /*
+    struct mazo jugador1;
     crear_piezas(jugador1);
-    printf("%d",*jugador1->pieza1);
-    
+    printf("%d",jugador1.pieza1);
+    */
+   int mazo1[7][2];
+   crear_piezas(mazo1);
+   printf("Creado el mazo1\n");
+   for (int i = 0; i < 7; i++){
+        printf("%d, %d\n", mazo1[i][0], mazo1[i][1]);
+   }
     return 0;
 }
