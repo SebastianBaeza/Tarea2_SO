3e446deaedd99e6ad4819047dcf5f20ea2898946
SebastianBaeza <sebastian.baezaa@usm.cl>
2022-10-30 23:31:14 -0300

    Logrado asignacion de mazo

--- probando.c
+++ probando.c
@@ -29,56 +29,55 @@ struct colocar{
 //quiero rellenar los mazos de los jugadores, el random es para que me de un numero del 0 al 27 para rellenar el mazo con la pieza
 //tablero dentro de pipe
 //en la posición del numero del arreglo piezas
-void crear_piezas(struct mazo *jugador){
-    int posicion = 0;
+void crear_piezas(struct mazo *jugador, int posicion[7]){
+    // int posicion = 0;
     int numero = 0;
     int flag = 0;
-    srand(time(NULL));
     while (flag == 0){
-        posicion = rand() % 27; //cambiar a 27
-        if (piezas[posicion][0] == -1){
-            continue;
-        }
+        // posicion = rand() % 27; //cambiar a 27
+        // if (piezas[posicion][0] == -1){
+        //     continue;
+        // }
         if (numero == 0) {
-            jugador->pieza1[0] = piezas[posicion][0];
-            jugador->pieza1[1] = piezas[posicion][1];
-            piezas[posicion][0] = -1;
+            jugador->pieza1[0] = piezas[posicion[0]][0];
+            jugador->pieza1[1] = piezas[posicion[0]][1];
+            // piezas[posicion][0] = -1;
             numero++;
         }
         else if (numero == 1) {
-            jugador->pieza2[0] = piezas[posicion][0];
-            jugador->pieza2[1] = piezas[posicion][1];
-            piezas[posicion][0] = -1;
+            jugador->pieza2[0] = piezas[posicion[1]][0];
+            jugador->pieza2[1] = piezas[posicion[1]][1];
+            // piezas[posicion[1]][0] = -1;
             numero++;
         }
         else if (numero == 2) {
-            jugador->pieza3[0] = piezas[posicion][0];
-            jugador->pieza3[1] = piezas[posicion][1];
-            piezas[posicion][0] = -1;
+            jugador->pieza3[0] = piezas[posicion[2]][0];
+            jugador->pieza3[1] = piezas[posicion[2]][1];
+            // piezas[posicion[1]][0] = -1;
             numero++;
         }
         else if (numero == 3){
-            jugador->pieza4[0] = piezas[posicion][0];
-            jugador->pieza4[1] = piezas[posicion][1];
-            piezas[posicion][0] = -1;
+            jugador->pieza4[0] = piezas[posicion[3]][0];
+            jugador->pieza4[1] = piezas[posicion[3]][1];
+            // piezas[posicion[1]][0] = -1;
             numero++;
         }
         else if (numero == 4){
-            jugador->pieza5[0] = piezas[posicion][0];
-            jugador->pieza5[1] = piezas[posicion][1];
-            piezas[posicion][0] = -1;
+            jugador->pieza5[0] = piezas[posicion[4]][0];
+            jugador->pieza5[1] = piezas[posicion[4]][1];
+            // piezas[posicion[1]][0] = -1;
             numero++;
         }
         else if(numero == 5){
-            jugador->pieza6[0] = piezas[posicion][0];
-            jugador->pieza6[1] = piezas[posicion][1];
-            piezas[posicion][0] = -1;
+            jugador->pieza6[0] = piezas[posicion[5]][0];
+            jugador->pieza6[1] = piezas[posicion[5]][1];
+            // piezas[posicion[1]][0] = -1;
             numero++;
         }
         else if(numero == 6){
-            jugador->pieza7[0] = piezas[posicion][0];
-            jugador->pieza7[1] = piezas[posicion][1];
-            piezas[posicion][0] = -1;
+            jugador->pieza7[0] = piezas[posicion[6]][0];
+            jugador->pieza7[1] = piezas[posicion[6]][1];
+            // piezas[posicion[1]][0] = -1;
             numero++;
             flag = 1;
         }
@@ -100,10 +99,10 @@ int main(){
     int pipe_p4[2];
     int pipe_4p[2];
     int* ptr = (int*)malloc(4 * sizeof(int));
-    ptr[0]=0;
-    ptr[1]=0;
-    ptr[2]=0;
-    ptr[3]=0;
+    ptr[0] = 0;
+    ptr[1] = 0;
+    ptr[2] = 0;
+    ptr[3] = 0;
 
     //Inicializar PIPES
     pipe(pipe_p1);
@@ -115,14 +114,35 @@ int main(){
     pipe(pipe_p4);
     pipe(pipe_4p);
 
+    int pos[28];
+    int var = 0;
+    int check = 0;
+    srand(time(NULL));
+
+    for (int a = 0; a < 28;){
+        var = rand() % 28;
+        for (int b = 0; b <= a; b++){
+            check = pos[b];
+            if (check == var){
+                break;
+            }
+        }
+        if (check == var){
+            continue;
+        } else {
+            pos[a] = var;
+            printf("Arreglo de posiciones, espacio %d = %d\n", a, pos[a]);
+            a++;
+        }
+    }
   
     // los mazos$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
-    struct mazo jugador1;
-    struct mazo jugador2;
-    struct mazo jugador3;
-    struct mazo jugador4;
+    struct mazo p1;
+    struct mazo p2;
+    struct mazo p3;
+    struct mazo p4;
 
-//hago los procesos$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+    // hago los procesos$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
     // value returned from fork() system call
     pid = fork();
   
@@ -140,59 +160,66 @@ int main(){
         // id and getppid() gives the
         // parent id of that process.
         proceso=0;
-
         
-        
-    }
-  
-    else {
+    } else {
         pid1 = fork();
         if (pid1 == 0) {
             proceso=1;
             //idem x=2;
 
-        }
-        else {
+        } else {
             pid2 = fork();
             if (pid2 == 0) {
                 proceso=2;
-            }
-            else{
+            } else {
                 pid3 = fork();
                 if (pid3 == 0) {
                     proceso=3;
-                }
-                else{
+                } else {
                     pid4 = fork();
-                if (pid4 == 0) {
-                    proceso=4;
-                }
-                    proceso=4;
+                    if (pid4 == 0) {
+                        proceso=4;
+                    }
+                    // proceso=4;
                 }
             
   
-            // If value returned from fork()
-            // in not zero and >0 that means
-            // this is parent process.
+                // If value returned from fork()
+                // in not zero and >0 that means
+                // this is parent process.
+            }
         }
     }
-    }
-    int i;
-//intento que interactuen entre sí los procesos, mas tarde lo extiendo a los otros$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
-    for (i=1;i<8;i++){
+    //intento que interactuen entre sí los procesos, mas tarde lo extiendo a los otros$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+    for (int i = 1; i < 8; i++){
         //if del proceso padre con whiles para cada hijo vrible que aumenta si todos juegan +1 
-        if(proceso==1){
+        if (proceso == 1){
             close(pipe_p1[0]); // cierro el modo de Lectura del padre al hijo
             close(pipe_1p[1]); // cierro el modo de Escritura del hijo al padre
 
-            int mensaje = 100;
+            int pos1[8];
+            memcpy(pos1,pos,sizeof(int)*7);
+
+            crear_piezas(&p1, &pos1);
+
+            printf("Creado el mazo\n");
+            printf("%d, %d\n", p1.pieza1[0], p1.pieza1[1]);
+            printf("%d, %d\n", p1.pieza2[0], p1.pieza2[1]);
+            printf("%d, %d\n", p1.pieza3[0], p1.pieza3[1]);
+            printf("%d, %d\n", p1.pieza4[0], p1.pieza4[1]);
+            printf("%d, %d\n", p1.pieza5[0], p1.pieza5[1]);
+            printf("%d, %d\n", p1.pieza6[0], p1.pieza6[1]);
+            printf("%d, %d\n", p1.pieza7[0], p1.pieza7[1]);
+
+
+            int mensaje = i * 100;
             write(pipe_p1[1], &mensaje, sizeof(int)); // Mensaje puesto en la PIPE del padre al hijo
             read(pipe_1p[0], &mensaje, sizeof(int));
 
             printf("la respuesta de mi hijo es: %d \n", mensaje);
             }
             //juegue
-        if(proceso==2){
+        if (proceso == 2){
             //juego
             close(pipe_p1[1]); // cierro el modo de Escritura del padre al hijo
             close(pipe_1p[0]); // cierro el modo de Lectura del hijo al padre
